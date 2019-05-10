71ac9847beca4f79d32697479f79c7bb551faa96
jimmosca <jaime.casado.a@gmail.com>
2019-05-10 13:21:51 +0200

    Completado footer, y scroll funcional de header

--- scroll.js
+++ scroll.js
@@ -1,9 +1,12 @@
-function myFunction() {
-  if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
+function hasReached(){
+  console.log(window.scrollY);
+  var element0 = document.getElementById('cabesa');
+  var element1 = document.getElementById('cabesaCont');
 
-		document.getElementById("contenedor").className = "borde1";
-  } else {
+  if (window.scrollY >= 500) {
+    element0.className = "d-flex flex-row  w-100 justify-content-center align-items-end head0";
+    
+  }else{
 
-		document.getElementById("contenedor").className = "borde";
   }
 }
